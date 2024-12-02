b1235807e1bb28cc3ca5be4c80a6c969f36042b4
Mohammed <mehmedkhan16@gmail.com>
2024-12-02 20:31:45 +0530

    test

--- components/WorkSlider.js
+++ components/WorkSlider.js
@@ -81,8 +81,8 @@ const WorkSlider = () => {
             <div className="grid grid-cols-2 grid-rows-2 gap-4 cursor-pointer">
               {slide.images.map((image, index)=> {
                 return (
-                <div className="relative rounded-lg overflow-hidden flex items-center justify-center group">
-                  <div className="flex items-center justify-center relative overflow-hidden group" key={index}>
+                <div className="relative rounded-lg overflow-hidden flex items-center justify-center group" key={index}>
+                  <div className="flex items-center justify-center relative overflow-hidden group" >
                     {/* Image */}
                     <Image src={image.path} width={500} height={300} alt="" />
                     {/* Overlay Gradient */}
